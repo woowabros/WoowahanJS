50c0ec8f4c14fce48ef7dbc7a227e8d9dd4a608e
shallaa <shallaa@shallaazm.com>
2016-06-29 07:49:21 +0000

    [view] updateView 에 인자가 없는 경우 스스로를 새로고침

--- examples/helloworld/hello-view.js
+++ examples/helloworld/hello-view.js
@@ -1,17 +1,38 @@
 import Woowahan from '../../';
 
 export default Woowahan.View.create('Index', {
-  template: '<h1>Hello, WoowahanJS</h1>',
+  template: '<h1>Hello, WoowahanJS</h1><p id="count"></p><button id="btn-update">updateView self</button>',
+  
+  events: {
+    'click #btn-update': 'onClickUpdate'
+  },
   
   initialize() {
+    this.setModel({ count: 0 });
+    
     this.super();
   },
   
   viewWillMount(renderData) {
-    console.log('viewWillMount');
+    console.log(renderData.count);
   },
   
   viewDidMount($el) {
-    $el.css('font-size', 50);
+    $el.find('h1').css('font-size', 50);
+    
+    $el.find('#count').text(this.getModel('count') + ' mount');
+  },
+  
+  viewWillUnmount() {
+    console.log('this view will unmount');
+  },
+  
+  onClickUpdate() {
+    let count = this.getModel('count');
+    
+    this.setModel({ count: ++count });
+    this.updateView();
+    
+    return false;
   }
 });
