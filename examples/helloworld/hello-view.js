18579f70b8c02592dc2c5af71ca95ba1cd3af5ed
Kim mintae <kim.mintae@woowahan.com>
2016-07-01 16:04:06 +0900

    helloworld 예제 수정 - 더욱 더 단순한 형태로 변경

--- examples/helloworld/hello-view.js
+++ examples/helloworld/hello-view.js
@@ -1,38 +1,5 @@
 import Woowahan from '../../';
 
-export default Woowahan.View.create('Index', {
-  template: '<h1>Hello, WoowahanJS</h1><p id="count"></p><button id="btn-update">updateView self</button>',
-  
-  events: {
-    'click #btn-update': 'onClickUpdate'
-  },
-  
-  initialize() {
-    this.setModel({ count: 0 });
-    
-    this.super();
-  },
-  
-  viewWillMount(renderData) {
-    console.log(renderData.count);
-  },
-  
-  viewDidMount($el) {
-    $el.find('h1').css('font-size', 50);
-    
-    $el.find('#count').text(this.getModel('count') + ' mount');
-  },
-  
-  viewWillUnmount() {
-    console.log('this view will unmount');
-  },
-  
-  onClickUpdate() {
-    let count = this.getModel('count');
-    
-    this.setModel({ count: ++count });
-    this.updateView();
-    
-    return false;
-  }
+export default Woowahan.View.create('Hello', {
+  template: '<h1>Hello, WoowahanJs</h1>'
 });
