c557a0742c2d54519896d91eda16f9f5b519c573
Kim mintae <kim.mintae@woowahan.com>
2016-07-06 16:23:02 +0900

    listview 셈플 코드 정리

--- examples/listview/view/users/user.js
+++ examples/listview/view/users/user.js
@@ -2,19 +2,8 @@ import Template from './user.hbs';
 import Woowahan from '../../../../';
 
 export default Woowahan.ItemView.create('User', {
-  namespace: 'View:Users:User',
   tagName: 'tr',
   template: Template,
-
-  initialize(data) {
-    this.setModel(data);
-    
-    this.super();
-  },
-  
-  viewWillUnmount() {
-    // console.log(this.getModel());
-  },
 
   onSelectedRow(event, trigger) {
     trigger({ id: this.getModel('id') });
