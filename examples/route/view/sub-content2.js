4f011e4216ba2435135091e01d8e8f2c3c1b2005
woowa <junho.lee@woowahan.com>
2017-09-06 18:07:20 +0900

    [route] updateView 버튼 추가

--- examples/route/view/sub-content2.js
+++ examples/route/view/sub-content2.js
@@ -2,5 +2,13 @@ import Woowahan from '../../../index';
 import Template from './sub-content2.hbs';
 
 export default Woowahan.View.create('SubContentView2', {
-  template: Template
+  template: Template,
+
+  events: {
+    'click .btn-update': 'onClickUpdate'
+  },
+
+  onClickUpdate() {
+    this.updateView();
+  }
 });
